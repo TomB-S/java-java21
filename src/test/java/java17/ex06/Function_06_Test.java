32d89e430f62d989d52dd25d3e860d0463094f66
TomB-S <thomas.benameur@gmail.com>
2026-07-24 09:45:13 +0200

    TP_03_2

--- src/test/java/java17/ex06/Function_06_Test.java
+++ src/test/java/java17/ex06/Function_06_Test.java
@@ -18,7 +18,8 @@ public class Function_06_Test {
     // TODO la méthode retourne une chaîne de caractères de la forme [age=<AGE>] (exemple : [age=12])
     String formatAge(Supplier<Person> supplier) {
         // TODO
-        return null;
+        Person person = supplier.get();
+        return "[age=" + person.getAge() + "]";
     }
     // end::formatAge[]
 
@@ -26,7 +27,7 @@ public class Function_06_Test {
     @Test
     public void test_supplier_formatAge() throws Exception {
         // TODO compléter le test unitaire pour qu'il soit passant
-        String result = formatAge(null);
+        String result = formatAge( () -> new Person("John", "Doe", 35, "azerty"));
 
         assert result.equals("[age=35]");
     }
